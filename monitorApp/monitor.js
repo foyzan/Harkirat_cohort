772098a27c48a1f2feebd53e0b987ad4c6d5722c
foyzan <foyzanahmedapon01@gmail.com>
2025-05-20 12:05:56 +0600

    System Monitoring CLI app
    CPU core monitoring in percentage

---
+++ monitorApp/monitor.js
@@ -0,0 +1,33 @@
+import os from 'node:os';
+import { cpuUsage } from 'node:process';
+
+function getSystemCPUInfo() {
+    const oldCPUs = os.cpus();
+
+    setTimeout(() => {
+        const newCPUs = os.cpus();
+        
+        const cpuUsage = newCPUs.map((cpu, index) => {
+
+            return{
+                core : index,
+                usage: calculateCPUUsage(oldCPUs[index], cpu) + '%',
+            }
+        })
+        console.clear();
+        console.table(cpuUsage, ["core", "usage"]);
+    }, 1000);
+}
+
+
+function calculateCPUUsage(oldCPUs, newCPUs) {
+    const oldTotal = Object.values(oldCPUs.times).reduce((acc, time) => acc + time);
+    const newTotal = Object.values(newCPUs.times).reduce((acc, time) => acc + time);
+    const idleDiff = newCPUs.times.idle - oldCPUs.times.idle;   
+    const totalDiff = newTotal - oldTotal;
+
+    const usage = ((totalDiff - idleDiff) / totalDiff) * 100;
+    return usage.toFixed(1);
+}
+
+setInterval(getSystemCPUInfo,1000)
