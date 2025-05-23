ecfb4bc41a004de173ae569a7bfd9d9ea11bde60
foyzan <foyzanahmedapon01@gmail.com>
2025-05-23 23:35:24 +0600

    using stream in node js

--- HttpServer/nodeServer.js
+++ HttpServer/nodeServer.js
@@ -1,9 +1,32 @@
-import {createServer} from 'node:http';
+import { createServer } from "node:http";
+import fs from "node:fs"
+const server = createServer( function (req, res) {
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+
+    const dataStream = fs.createReadStream("./index.html")
+
+    dataStream.on('data', function(chunk){
+        res.write(chunk)
+    })
+
+    dataStream.on('end', function(){
+        res.end()
+    })
 
 
-const server = createServer(function(req, res){
 
-    console.log("request received")
+    
+  } 
+  else if (req.url === "/about"){
+     
+    res.writeHead(200, { "content-type": "text/html" });
 
-    res.end("Hello node Server")
-})
+     res.end("<h1>about page</h>");
+
+  }
+});
+
+server.listen(4000, function () {
+  console.log("server is running on porn 4000");
+});
