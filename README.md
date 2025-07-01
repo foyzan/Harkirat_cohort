90d8cc8a0e4c784638de442947b37d2d29ba63ac
foyzan <foyzanahmedapon01@gmail.com>
2025-07-01 17:50:43 +0600

    Initial commit or your message describing the changes

---
+++ README.md
@@ -0,0 +1,7 @@
+## TODO app
+
+A simpl TODO application to understand fullstack work flow:
+
+- anyone can create todo
+- anyone can see their todos
+- anyone can mark a todo as done
---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express()
+app.listen(3000, function(){
+    console.log('server is running ' + 3000);  
+})
+
+
+
+app.use(express.json())
+
+
+app.get("/todo", function(req, res){
+
+    res.send("<h1>hello<h1/>")
+})
+
+app.post("/todos", function(req, res){
+
+})
+
+app.put("/completed", function(req, res){
+
+})
